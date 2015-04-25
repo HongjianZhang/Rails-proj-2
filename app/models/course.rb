4c6686793661931ccf2c8c9cf29ac77b3e267518
Kanstantsin Kastsevich <kkastsevich@gmail.com>
2015-04-25 13:31:15 -0700

    start my work

---
+++ app/models/course.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id           :integer          not null, primary key
+#  courseNumber :string(255)
+#  title        :string(255)
+#  department   :string(255)
+#  description  :text
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
+class Course < ActiveRecord::Base
+end
